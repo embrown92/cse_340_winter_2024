830d89f40e175e4335a4138089bebecd3191b6b2
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-02-03 01:01:27 -0500

    Build inventory route, controller, model and classification view

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -3,11 +3,11 @@ const pool = require("../database/")
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
+async function getClassifications() {
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
-module.exports = {getClassifications}
+module.exports = { getClassifications }
 
 /**********************
 An Explanation
@@ -29,5 +29,24 @@ Line 7 - will return (send back) the result of the SQL query, which will be sent
 Line 8 - ends the function began on line 6.
 Line 9 - left intentionally blank.
 Line 10 - exports the function for use elsewhere. 
-
 ********************** */
+
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM inventory AS i 
+        JOIN classification AS c 
+        ON i.classification_id = c.classification_id 
+        WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+module.exports = { getClassifications, getInventoryByClassificationId };
--- utilities/index.js
+++ utilities/index.js
@@ -11,7 +11,7 @@ Util.getNav = async function (req, res, next) {
   data.rows.forEach((row) => {
     list += "<li>"
     list +=
-      '<a href="/inv/type/' +
+      '<a href="/inv/type/#' +
       row.classification_id +
       '" title="See our inventory of ' +
       row.classification_name +
@@ -52,3 +52,36 @@ Line 25 - the closing HTML unordered list tag being appended to the list variabl
 Line 26 - sends the finished string, back to where the getNav function was called.
 Line 27 - ends the function which started on line 7.
  * *********************** */
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
