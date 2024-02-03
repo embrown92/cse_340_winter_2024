8fccae6732c44e233b0b744916184195fca68aaa
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-02-03 01:07:10 -0500

    modified public count type owner to

--- database/assignment1.sql
+++ database/assignment1.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340db;
+    OWNER TO databasecse340;
 
 -- Table structure for table `classifaction`
 CREATE TABLE public.classification (
